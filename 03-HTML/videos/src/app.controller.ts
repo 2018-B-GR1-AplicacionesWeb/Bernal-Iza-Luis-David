7651b12683a0bff44cb67c74aad9f8c19e46f641
ludabeiz <david.15_@hotmail.com>
2018-12-11 22:43:30 -0500

    front

--- 03-HTML/videos/src/app.controller.ts
+++ 03-HTML/videos/src/app.controller.ts
@@ -1,12 +1,171 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+    Body,
+    Controller,
+    Get,
+    Headers,
+    HttpCode,
+    InternalServerErrorException,
+    Param,
+    Post,
+    Query,
+    Req,
+    Res,
+    UnauthorizedException
+} from '@nestjs/common';
+import {AppService} from './app.service';
+import {Observable, of} from "rxjs";
+import {Request, Response} from "express";
 
-@Controller()
+@Controller()  //decoradores
+// Controller('usuario')
+// http://localhost:3000/usuario
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    // public servicio:AppService;
+    constructor(private readonly _appService: AppService) {  // NO ES UN CONSTRUCTOR
+        // this.servicio = servicio;
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+
+    @Get() // http://ip:puerto
+    // @Get('crear')
+    // http://localhost:3000/usuario/crear?nombre=Adrian
+    @HttpCode(204) // status
+    raiz(
+        @Query() todosQueryParams: any,  //{nombre:"Adrian"}
+        @Query('nombre') nombre: string, // adrian
+    ): string {
+        console.log(todosQueryParams);
+        return 'Hola Mundo' + nombre;
+    }
+
+    @Get('segmentoUno/segmentoDos/:idUsuario')  // PARAMETRO RUTA
+    // http://localhost:3000/usuario/segmentoUno/segmentoDos/10
+    parametroRuta(
+        @Param('idUsuario') id
+    ) {
+        return id;
+    }
+
+    @Get('adiosMundo') // url
+    adiosMundo(): string {
+        return 'Adios Mundo';
+    }
+
+    @Post('adiosMundo') // url
+    adiosMundoPOST(): string {
+        return 'Adios Mundo POST';
+    }
+
+    @Get('adiosMundoPromesa') // url
+    adiosMundoPromesa(): Promise<string> {
+        const promesaAdios = (): Promise<string> => {
+            return new Promise(
+                (resolve) => {
+                    resolve('Adios Mundo');
+                }
+            )
+        };
+        return promesaAdios();
+    }
+
+
+    @Get('adiosMundoAsync') // url
+    @HttpCode(201)
+    async adiosMundoAsync() {
+        const promesaAdios = (): Promise<string> => {
+            return new Promise(
+                (resolve, reject) => {
+                    reject('Adios Mundo');
+                }
+            )
+        };
+        try {
+            const respuesta: string = await promesaAdios();
+            return respuesta;
+        } catch (e) {
+            console.error(e);
+            throw new InternalServerErrorException({mensaje: 'Error servidor'})
+        }
+
+    }
+
+    @Get('adiosMundoObservable') // url
+    adiosMundoObservable(): Observable<string> {
+        const respuesta$ = of('Adios Mundo');
+        return respuesta$;
+    }
+
+    @Post('crearUsuario')
+    @HttpCode(200)  // Codigo OK
+    crearUsuario(
+        @Body() usuario: Usuario,
+        @Body('nombre') nombre: string,
+        @Headers() cabeceras, // Cabeceras de peticion,
+        @Headers('seguridad') codigo, // Cabeceras de peticion
+        @Res() res: Response,
+        @Req() req: Request | any,
+    ) {
+        // crear usuario
+        console.log('Cookies', req.cookies);  // LEIDO
+        console.log('Cookies', req.secret);
+        console.log('Cookies Seguras', req.signedCookies);  // LEIDO
+        console.log(usuario);
+        console.log(cabeceras);
+
+        if (codigo === '1234') {
+
+            const bdd = this._appService.crearUsuario(usuario);
+
+            res.append('token', '5678'); // AQUI
+            res.cookie("app", "web"); // INSEGURA
+            res.cookie("segura", "secreto", {
+                signed: true
+            });
+
+            res.json(bdd);
+
+        } else {
+            throw new UnauthorizedException({  // MALO
+                mensaje: 'Error de autorizacion',
+                error: 401
+            })
+        }
+
+
+    }
+
+    @Get('inicio')
+    inicio(
+        @Res() response,
+    ){
+        response.render(
+            'inicio',
+            {
+                usuario:'David',
+                arreglo:[
+                    {
+                        titulo:'A',
+                        descripcion:'asd asda asd asd asd'
+                    },
+                    {
+                        titulo:'B',
+                        descripcion:'asd asda asd asd asd'
+                    },
+                    {
+                        titulo:'C',
+                        descripcion:'asd asda asd asd asd'
+                    }
+                ],
+                boolean:true,
+            }
+        );
+    }
+
 }
+
+
+export interface Usuario {
+    nombre: string;
+}
+
+// http://localhost:3000
